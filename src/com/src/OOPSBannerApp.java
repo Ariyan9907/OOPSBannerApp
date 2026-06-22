54459960ad1e09116fb323d283f98a15654fbb06
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-22 21:42:29 +0530

    UC OOPS Banner Added

--- src/com/src/OOPSBannerApp.java
+++ src/com/src/OOPSBannerApp.java
@@ -2,6 +2,12 @@ package com.src;
 
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println(" ***     ***    ******    ***** ");
+        System.out.println("*   *   *   *   *     *  *      ");
+        System.out.println("*   *   *   *   *     *  *      ");
+        System.out.println("*   *   *   *   ******    ****  ");
+        System.out.println("*   *   *   *   *             * ");
+        System.out.println("*   *   *   *   *             * ");
+        System.out.println(" ***     ***    *        *****  ");
     }
 }
