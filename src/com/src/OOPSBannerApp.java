d4625fe235280474de3d4dd3fb3bc7b816fc667e
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-22 23:18:31 +0530

    Seperate method is created for each aphabet

--- src/com/src/OOPSBannerApp.java
+++ src/com/src/OOPSBannerApp.java
@@ -1,16 +1,50 @@
 package com.src;
 
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-        String lines[]= {
-                String.join("   ", " ***** ", " ***** ", "*******", " ******"),
-        String.join("   ", "*     *", "*     *", "*      *", "*      "),
-        String.join("   ", "*     *", "*     *", "*******", " ***** "),
-        String.join("   ", "*     *", "*     *", "*      ", "      *"),
-        String.join("   ", " ***** ", " ***** ", "*      ", "****** "),
+
+    public static String[] getOPattern() {
+        return new String[]{
+                " **** ",
+                "*    *",
+                "*    *",
+                "*    *",
+                " **** "
         };
-        for(String x:lines){
-            System.out.println(x);
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "***** ",
+                "*    *",
+                "***** ",
+                "*     ",
+                "*     "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                    o[i] + "   " +
+                            o[i] + "   " +
+                            p[i] + "   " +
+                            s[i]
+            );
         }
     }
 }
